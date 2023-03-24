3cabe815779e78f869ff3d2b6a0cf7324a5d8c2c
Raphaël <raphael.nk.dev@gmail.com>
2023-03-24 12:45:27 +0300

    feat: added swagger docs

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
+// const swaggerDocument = require('./swagger.json');
 
 const app = express();
 app.use(express.json());
@@ -23,11 +23,29 @@ mongoose.connect(MONGODB_CONNECTION, {
 .catch(err => console.log(err));
 
 // ROUTE API
-app.get('/', async (req, res, next) => {
-  res.send({ message: 'Awesome it works 🐻' });
-});
-
 app.use('/api', require('./routes/api.route'));
+ 
+// SETUP SWAGGER
+const swaggerJsdoc = require('swagger-jsdoc');
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'API SWAGGER HIU 2K23',
+      version: '1.0.0',
+      description: 'Team Yasai',
+    },
+    servers: [
+      {
+        url: 'http://localhost:3333',
+      },
+    ],
+  },
+  apis: ['./routes/*.js'], 
+};
+
+const swaggerSpec = swaggerJsdoc(options);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec)); 
 
 app.use((req, res, next) => {
   next(createError.NotFound());
@@ -40,9 +58,6 @@ app.use((err, req, res, next) => {
     message: err.message,
   });
 });
-
-// setup Swagger
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`🚀 @ http://localhost:${PORT}`));
--- routes/api.route.js
+++ routes/api.route.js
@@ -1,7 +1,57 @@
 const router = require('express').Router();
 
+/**
+ * @swagger
+ * /api:
+ *   get:
+ *     summary: Get a message
+ *     description: Retrieve a list of String
+ *     responses:
+ *       '200':
+ *         description: A list of users
+ */
 router.get('/', async (req, res, next) => {
   res.send({ message: 'Ok api is working 🚀' });
+});
+
+/**
+ * @swagger 
+ * /api/users/{userID}:
+ *  post:
+ *     summary: Create a new user
+ *     parameters:
+ *       - in: path
+ *         name: userID
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The ID of the user to create
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               id:
+ *                 type: string
+ *                 description: The ID of the user
+ *               name:
+ *                 type: string
+ *                 description: The name of the user
+ *             required:
+ *               - id
+ *               - name
+ *     responses:
+ *       '201':
+ *         description: Created
+ *       '400':
+ *         description: Bad request
+ *       '500':
+ *         description: Internal server error
+ * */ 
+router.post('/users', async (req, res, next) => {
+  res.send({ message: 'New user 🚀' });
 });
 
 module.exports = router;
